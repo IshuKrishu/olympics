0e84f875923854afb3ea52abda03e117670a8f8c
IshuKrishu <92384951+IshuKrishu@users.noreply.github.com>
2022-06-01 20:57:39 +0530

    Add files via upload

---
+++ mani.js
@@ -0,0 +1,45 @@
+canvas = document.getElementById("olympics");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "grey" ;
+ctx.lineWidth= 4;
+ctx.rect(200, 145 , 350 , 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue" ;
+ctx.lineWidth= 5;
+ctx.arc(290, 210 , 40 , 0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow" ;
+ctx.lineWidth= 5;
+ctx.arc(340, 250 , 40 , 0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth=5;
+ctx.arc(390,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth=5;
+ctx.arc(440,250,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth=5;
+ctx.arc(490,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+
+  
+
+
+
+
